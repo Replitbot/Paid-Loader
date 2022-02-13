a04ddc4daf7995d1300be190a0f3e92354160117
Replitbot <97874310+Replitbot@users.noreply.github.com>
2022-02-14 05:57:54 +0800

    Create paid loader.lua

---
+++ paid loader.lua
@@ -0,0 +1,6 @@
+if game.GameId == 7842028704 then -- Anime Clash
+    a = "https://raw.githubusercontent.com/Replitbot/ASC-PAID-VER/main/PAID%20VERSION"
+elseif game.PlaceId == 7114303730 then -- Dragon Ball Orbz
+    a = "https://raw.githubusercontent.com/Replitbot/Dragon-Orbz/main/dragon%20orbs.lua"
+end
+    loadstring(game:HttpGet(a))()
