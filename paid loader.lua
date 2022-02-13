0f5da794752e2f7d4bd1a567e0ae6142ee2e23f8
Replitbot <97874310+Replitbot@users.noreply.github.com>
2022-02-14 07:34:00 +0800

    Update paid loader.lua

--- paid loader.lua
+++ paid loader.lua
@@ -1,4 +1,4 @@
-if game.GameId == 7842028704 then -- Anime Clash
+if game.PlaceId == 7842028704 then -- Anime Clash
     a = "https://raw.githubusercontent.com/Replitbot/ASC-PAID-VER/main/PAID%20VERSION"
 elseif game.PlaceId == 7114303730 then -- Dragon Ball Orbz
     a = "https://raw.githubusercontent.com/Replitbot/Dragon-Orbz/main/dragon%20orbs.lua"
